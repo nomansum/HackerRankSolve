edff6cf12c9cb1d6073b9111bacf4164782a50bf
Shibli Noman <63536302+nomansum@users.noreply.github.com>
2023-06-27 16:50:08 +0600

    Diagonal Difference

---
+++ DiagonalMatrixSumDif.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    length = len(arr)
+    result = 0
+    for i in range(length):
+        result += arr[i][i]
+    
+    j = length-1
+        
+    for i in range(length):
+        result -= arr[j][i]
+        j-=1
+    
+    result = abs(result)
+    return result 
+    
+    
+        
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
